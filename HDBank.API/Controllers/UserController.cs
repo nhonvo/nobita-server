1874a50fc6cc973a5839a030141e99cc957c03f1
Slimaeus <nguyenhongthai28042002@gmail.com>
2022-11-23 13:03:03 +0700

    Add API and Infrastructure

---
+++ HDBank.API/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HDBank.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Authorize()
+        {
+            return Ok();
+        }
+    }
+}
